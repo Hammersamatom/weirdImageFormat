c8baac6f609d8d54ca16ee701bdaedc1e080d2d7
Hammersamatom <hammersamatom@gmail.com>
2024-04-09 18:25:54 -0400

    Initial Commit
    This is a program does the following:
    * Takes in a raw RGBA (8bits per color) file, converted from a paletted
      image containing only 16 colors
    * Converts the occurances of RGBA pixels from the file into an index
      array
    * Run-Length Encodes the resulting index array, resulting in a
      significantly reduced file size
    
    The encoding scheme will we described in the upcoming README.md, and
    only supports 16 colors at this time.

---
+++ src/main.cpp
@@ -0,0 +1,70 @@
+#include <fstream>
+#include <iterator>
+#include <strings.h>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <iostream>
+#include <fmt/core.h>
+
+#include "rle.hpp"
+
+template <typename T>
+void fill_vector(std::vector<T>& vec, std::ifstream& file)
+{
+	file.seekg(0, file.end);
+	std::size_t size = file.tellg();
+	file.seekg(file.beg);
+
+	for (std::size_t i = 0; i < size/sizeof(T); i++)
+	{
+		T temporary_value;
+		file.read((char*)&temporary_value, sizeof(T));
+		vec.push_back(temporary_value);
+	}
+}
+
+int main(int argc, char** argv)
+{
+	if (argc < 2)
+		fmt::println(stderr, "Usage: {} INPUT OUTPUT", argv[0]);
+
+	std::ifstream file_in(argv[1], std::ios::binary);
+	if (!file_in.is_open())
+	{
+		fmt::print(stderr, "Failed to open file!\n");
+		return 1;
+	}
+
+	std::vector<uint32_t> pixel_data;
+	fill_vector<uint32_t>(pixel_data, file_in);	
+
+	fmt::print(stderr, "Size {}\n", pixel_data.size());
+	std::map<uint32_t, uint8_t> color_index;
+
+	for (auto& p : pixel_data)
+	{
+		color_index[p];
+	}
+
+	std::size_t index = 0;
+	for (auto& c : color_index)
+	{
+		c.second = index++;
+	//	fmt::println("Color: {:08X}, Count: {}", c.first, c.second);
+	}
+	
+	//fmt::println("Number of colors: {}", color_index.size());
+
+	std::vector<uint8_t> indexes;
+	for (auto& p : pixel_data)
+	{
+		indexes.push_back(color_index[p]);
+	//	fmt::print("{:c}", color_index[p]);
+	}
+
+	auto rle = rle_encode(indexes);
+	std::copy(rle.begin(), rle.end(), std::ostream_iterator<uint8_t>(std::cout));
+	//std::copy(indexes.begin(), indexes.end(), std::ostream_iterator<uint8_t>(std::cout));
+	return 0;
+}
